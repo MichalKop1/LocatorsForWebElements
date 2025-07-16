cd4a5602574d2425b927d2e0adcea65af42e6d93
Michal Kopryk <michakopryk@gmail.com>
2025-07-16 22:18:03 +0200

    Fixed reference issues

--- Business/Pages/SearchPage.cs
+++ Business/Pages/SearchPage.cs
@@ -1,12 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Business.Pages;
 
